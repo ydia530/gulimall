8a37a47150d33728a6e4355bc9f89b5dded00811
ydia530 <55938375+ydia530@users.noreply.github.com>
2022-01-11 19:03:09 +0800

    oss客户端导入

--- gulimall-third-party/src/test/java/com/atguigu/gulimal/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimal/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -2,6 +2,7 @@ package com.atguigu.gulimal.thirdparty;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootTest
 class GulimallThirdPartyApplicationTests {
